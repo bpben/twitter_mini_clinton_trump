3a33f245f66240006da841002e97ec583e862035
bpben <B@Bens-MacBook-Pro.local>
2017-01-03 18:25:12 -0500

    edit for general use

--- timeline_clinton_trump.py
+++ timeline_clinton_trump.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov 07 22:21:42 2016
-#Retrieves last x tweets on Clinton/Trump timelines
+#Retrieves tweets on Clinton/Trump timelines
 
 @author: bpben
 """
@@ -25,24 +25,27 @@ auth=OAuth(token, token_secret, consumer_key, consumer_secret)
 
 tauth = Twitter(auth = auth)
 
-tweetSets = {}
-tweetSets['realDonaldTrump'] = set()
-tweetSets['HillaryClinton'] = set()
+users = ['realDonaldTrump', 'HillaryClinton']
 
-#Get most recent x tweets (max is 200)
-x = 200
-for u in ['realDonaldTrump','HillaryClinton']:
-    new = tauth.statuses.user_timeline(screen_name=u, include_rts=True, count=x)
+def gettimeline(user, num):
+    if num>200:
+        print('Specified number of tweets too high, defaulting to max (200)')
+        num = 200
+    tweetset = set()
+    data = []
+    print('Retrieving most recent {} tweets for {}').format(num,user)
+    new = tauth.statuses.user_timeline(screen_name=u, 
+        include_rts=True, count=num)
     for t in new:
-        if t['id'] not in tweetSets[u]:                
-            if os.path.isfile('./'+u+'.pkl'):
-                with open('./'+u+'.pkl') as f:
-                    data = cPickle.load(f)
-                data.append(t)
-                print "Have {} tweets".format(len(data))
-            else:
-                data = []                    
-                data.append(t)
-            with open('./'+u+'.pkl', 'w') as f:
-                cPickle.dump(data, f)
-            tweetSets[u].add(t['id'])
+        if t['id'] not in tweetset:
+            data.append(t)
+    print('Retrieved {} tweets').format(len(data))
+    return(data)
+
+def savepickle(data, user, filename): 
+    with open('./'+user+'.pkl', 'w') as f:
+        data = cPickle.dump(data, filename)
+
+for u in users:
+    data = gettimeline(u, 200)
+    savepickle(data, u, u+'.pkl')
